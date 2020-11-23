09e771a7434e93f60568b687d4db8c1bd8a4a825
Nitin <nitinvardha09@gmail.com>
2020-11-23 23:45:31 +0530

    added extension

---
+++ frontend/build/precache-manifest.3a91099b0f3af96f3d3f76d64d3f4253.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b8321804ded6b4dce96ae8376728b20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8bbff2231fad2bbc610",
+    "url": "/static/css/2.b52af277.chunk.css"
+  },
+  {
+    "revision": "150283c25bc630e780f3",
+    "url": "/static/css/main.a546dce9.chunk.css"
+  },
+  {
+    "revision": "b8bbff2231fad2bbc610",
+    "url": "/static/js/2.6a2e36a6.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.6a2e36a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "150283c25bc630e780f3",
+    "url": "/static/js/main.af33d56a.chunk.js"
+  },
+  {
+    "revision": "aff20233fc33123ab008",
+    "url": "/static/js/runtime-main.74f98a6f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
